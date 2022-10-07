d90898d3d6ba36866d90200e74c962740a6def77
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2022-10-07 11:53:21 +0100

    nuevos scripts

--- bash/menu-instalacion.sh
+++ bash/menu-instalacion.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 if which cowsay > /dev/null
 then
-	echo "Cowsay está instalado"
+	echo "Cowsay instalado"
 else
-	echo "Cowsay no está instalado. ¿Quieres instalarlo?"
+	echo "Cowsay no instalado"
+fi
+
+if which ifconfig > /dev/null
+then
+	echo "Net-Tools instaladas"
+else
+	echo "Not-Tools no instaladas"
 fi
