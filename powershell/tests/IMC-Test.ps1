072bfb06768bc608ca3c8bc68b67497a257c66b1
fvarrui <4354486+fvarrui@users.noreply.github.com>
2022-12-19 16:37:25 +0000

    Create IMC-Test.ps1

---
+++ powershell/tests/IMC-Test.ps1
@@ -0,0 +1,21 @@
+﻿Param(
+    [Parameter(Mandatory=$true)][string] $Script
+)
+
+Write-Host "`n********************************************************************************"
+Write-Host "* TESTEANDO SCRIPT $script"
+
+if (-not (Test-Path -Path $Script -PathType Leaf)) {
+    Write-Host "Script no encontrado"
+    exit 1
+}
+
+Write-Host "`n================================================================================"
+Write-Host "TEST 1: Calculando IMC con parámetros"
+Write-Host "================================================================================`n"
+& .\$Script -Peso 80 -Altura 175
+
+Write-Host "`n================================================================================"
+Write-Host "TEST 2: Calculando IMC con entrada estándar"
+Write-Host "================================================================================`n"
+& { "80`n175" | powershell -noprofile -command .\$Script }
