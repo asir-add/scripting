57e63ed6c8915b6128dd6883e66adc632f9e30cd
fvarrui <4354486+fvarrui@users.noreply.github.com>
2023-11-27 12:59:25 +0000

    ejecutar comandos desde python

--- python/listar.py
+++ python/listar.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
-import os
-salida=os.system("ls -l")
+import subprocess
 
-import math
-print(math.sqrt(9))
+def run(cmd):
+    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.DEVNULL)
+    return output.decode().strip()
+
+def print_enum(str):
+    for no, line in enumerate(str.split("\n"), 1):
+        print(f"{no:4}: {line}")
+
+output = run("ls -l | tail -n +2")
+print_enum(output)
