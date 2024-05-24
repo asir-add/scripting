d432764e2a82c5fca590c4ed005db56af361a9ee
fvarrui <4354486+fvarrui@users.noreply.github.com>
2024-05-24 12:41:52 +0100

    cambios

--- bash/existe.sh
+++ bash/existe.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-######################################
-# Comprobar si un fichero existe
-######################################
-fichero=$1
-if [ ! -f $fichero ]
+prog=$0
+usuario=$1
+
+[ $USER != root ] && echo "Debes ser root" && exit 1
+
+# comprobamos que el número de parámetros sea 1
+if [ $# -ne 1 ]
 then
-	echo El fichero $fichero NO existe.
+	echo "Debe especificar el nombre de usuario"
+	echo "Uso: $prog usuario"
+	exit 1
+fi
+
+# busca $usuario en el fichero /etc/passwd
+if cat /etc/passwd | grep -q "^$usuario:"
+then
+	echo "$usuario existe!!! ;-D"
+else
+	echo "$usuario NO existe :_("
 fi
